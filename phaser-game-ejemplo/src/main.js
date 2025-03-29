ebff9dd5b0b4c9ce0d167662325c806db13fa087
MiguelNotario <annihilux20@gmail.com>
2025-03-29 10:45:27 +0100

    Puesta al día de lo que hicimos en clase.

--- phaser-game-ejemplo/src/main.js
+++ phaser-game-ejemplo/src/main.js
@@ -1,20 +1,27 @@
-import { Game as MainGame } from './scenes/Game';
+import { Game as JuegoPrincipal } from './scenes/Game';
 import { AUTO, Scale,Game } from 'phaser';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
     type: AUTO,
-    width: 1024,
-    height: 768,
+    width: 800,
+    height: 600,
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    //backgroundColor: '#028af8',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scale: {
         mode: Scale.FIT,
         autoCenter: Scale.CENTER_BOTH
     },
     scene: [
-        MainGame
+        JuegoPrincipal
     ]
 };
 
--- phaser-game-ejemplo/src/scenes/Game.js
+++ phaser-game-ejemplo/src/scenes/Game.js
@@ -2,10 +2,12 @@ import { Scene } from 'phaser';
 
 export class Game extends Scene
 {
+ 
     constructor ()
     {
         super('Game');
     }
+
 
     preload ()
     {
@@ -13,18 +15,34 @@ export class Game extends Scene
         
         this.load.image('background', 'bg.png');
         this.load.image('logo', 'logo.png');
+
+        this.load.image('sky', 'sky.png');
+        this.load.image('ground', 'platform.png');
+        this.load.image('star', 'star.png');
+        this.load.image('bomb', 'bomb.png');
+        this.load.spritesheet('dude', 'dude.png', { frameWidth: 32, frameHeight: 48 });
     }
 
     create ()
     {
+        this.add.image(400, 300, 'sky');
+
+        this.platforms=this.physics.add.staticGroup();
+
+        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');
         
+        /*
         this.add.image(512, 384, 'background');
         this.add.image(512, 350, 'logo').setDepth(100);
         this.add.text(512, 490, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+        }).setOrigin(0.5).setDepth(100);*/
         
     }
 }
