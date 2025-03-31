8ffefcbd9f806915742e2c8d9711a3c1f531783d
Annihilux <annihilux20@gmail.com>
2025-03-31 09:10:47 +0200

    Dia 31 Marzo - phaser

--- phaser-game-ejemplo/src/scenes/Game.js
+++ phaser-game-ejemplo/src/scenes/Game.js
@@ -27,14 +27,45 @@ export class Game extends Scene
     {
         this.add.image(400, 300, 'sky');
 
-        this.platforms=this.physics.add.staticGroup();
+        this.suelos=this.physics.add.staticGroup();
 
-        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+        this.suelos.create(400, 568, 'ground').setScale(2).refreshBody();
 
-        this.platforms.create(600, 400, 'ground');
-        this.platforms.create(50, 250, 'ground');
-        this.platforms.create(750, 220, 'ground');
+        this.suelos.create(600, 400, 'ground');
+        this.suelos.create(50, 250, 'ground');
+        this.suelos.create(750, 220, 'ground');
+
+        this.player = this.physics.add.sprite(100, 450, 'dude');
+
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.player.body.setGravityY(30);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
         
+        this.physics.add.collider(this.player, this.suelos);
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+
         /*
         this.add.image(512, 384, 'background');
         this.add.image(512, 350, 'logo').setDepth(100);
@@ -44,5 +75,32 @@ export class Game extends Scene
             align: 'center'
         }).setOrigin(0.5).setDepth(100);*/
         
+    }
+
+    update(){
+        if (this.cursors.left.isDown)
+            {
+                this.player.setVelocityX(-160);
+            
+                this.player.anims.play('left', true);
+            }
+            else if (this.cursors.right.isDown)
+            {
+                this.player.setVelocityX(160);
+            
+                this.player.anims.play('right', true);
+            }
+            else
+            {
+                this.player.setVelocityX(0);
+            
+                this.player.anims.play('turn');
+            }
+            
+            if (this.cursors.up.isDown && this.player.body.touching.down)
+            {
+                this.player.setVelocityY(-330);
+            }
+
     }
 }
